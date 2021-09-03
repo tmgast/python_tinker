bf7d18d5ff471743cacfd9bb9d65cd8b08c042b1
= <tmgast@gmail.com>
2021-09-03 12:33:49 +0900

    project initialization

---
+++ printing.py
@@ -0,0 +1,18 @@
+date = "now"
+print(date)
+
+date = "and then, now"
+print(date)
+
+maths = 3 + 4
+print(maths)
+
+maths += 5
+print(maths)
+
+def ham():
+    print("going ham")
+    return ("done hamming it up")
+
+print("thinking")
+print(ham())
